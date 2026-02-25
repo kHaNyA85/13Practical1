cc856cc86042ebc22ec518652b0fe73e9aa1364b
kHaNyA85 <4531795@myuwc.ac.za>
2026-02-25 16:01:46 +0200

    Implement Node class and linear search method
    Added a Node class and a linear search method to the timeMethods class.

--- 4531795_13Practical.java
+++ 4531795_13Practical.java
@@ -3,3 +3,25 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+
+
+public class timeMethods {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }`
