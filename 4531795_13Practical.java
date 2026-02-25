a8ff5d436149b9291475901c00026e8b46a8af74
kHaNyA85 <4531795@myuwc.ac.za>
2026-02-25 16:03:26 +0200

    Implement binary search and random key generator
    Added binary search and random key generation methods.

--- 4531795_13Practical.java
+++ 4531795_13Practical.java
@@ -24,4 +24,31 @@ public class timeMethods {
             }
         }
         return -1;
-    }`
+    }
+
+
+public static int binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int[] generateRandomKeys(int count, int upperBound) {
+        Random rand = new Random();
+        int[] keys = new int[count];
+        for (int i = 0; i < count; i++) {
+            keys[i] = rand.nextInt(upperBound);
+        }
+        return keys;
+    }
